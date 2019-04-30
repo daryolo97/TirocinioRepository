d6475449ed0230e0bdc49a00fb0d0a1e3e9aadd1
daryolo97 <ddesimone97@gmail.com>
2019-04-30 15:54:54 +0200

    1.1.1

---
+++ laCulturaCheNonC'è/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	public static void main(String[] args) {
+		Visitatore v = new Visitatore("DRDMAH931D", "dario@hotmail.it");
+		MuseoBorbonico museoBorbonico = new MuseoBorbonico("1", "museo", 10, 15);
+		
+		museoBorbonico.visita(v);
+	}
+
+}
