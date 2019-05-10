3558a548707d49b210b0e7bfa95f06dea93357ee
daryolo97 <ddesimone97@gmail.com>
2019-05-10 15:17:20 +0200

    E' stato aggiunto un nuovo tipo di museo

---
+++ laCulturaCheNonC'è/src/MuseoGotico.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class MuseoGotico extends Museo{
+	public MuseoGotico(String codiceID, String nome, int prezzo, ArrayList<Visitatore> visitatori, int tasseRiscosse, int nMaxVisitatori) {
+		super(codiceID, nome);
+		this.prezzo = prezzo;
+		this.tasseRiscosse = tasseRiscosse;
+		this.nMaxVisitatori = nMaxVisitatori;
+	}
+	
+	public void visita(Visitatore v) {
+		if(visitatori.size() <= nMaxVisitatori) 
+			visitatori.add(v);
+		else 
+			throw new IllegalArgumentException("RAGGIUNTO NUMERO MASSIMO VISITE");	
+		
+		tasseRiscosse+=prezzo;
+		v.pagamento(prezzo);
+	}
+	
+	
+	private int prezzo, tasseRiscosse, nMaxVisitatori;
+	private ArrayList<Visitatore> visitatori;
+	
+
+}
