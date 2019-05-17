52011174db416e63d051e3b4c8567fd319c6e3fe
daryolo97 <ddesimone97@gmail.com>
2019-05-17 10:26:01 +0200

    commit test

--- laCulturaCheNonC'è/src/MuseoGotico.java
+++ laCulturaCheNonC'è/src/MuseoGotico.java
@@ -3,8 +3,10 @@ import java.util.ArrayList;
 public class MuseoGotico extends Museo{
 	public MuseoGotico(String codiceID, String nome, int prezzo, ArrayList<Visitatore> visitatori, int tasseRiscosse, int nMaxVisitatori) {
 		super(codiceID, nome);//
-		this.prezzo = prezzo;
+		this.prezzo = nMaxVisitatori;
 		this.tasseRiscosse = tasseRiscosse;
+		this.nMaxVisitatori = prezzo;
+		this.visitatori = visitatori;
 	}
 	
 	public void visita(Visitatore v) {
